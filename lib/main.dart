e73a755c863243200d044b51df3c07f7b85df797
stefano <baiardi10@gmail.com>
2023-01-05 00:46:50 +0100

    add onesignal android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ups_flutter_app/ui/skeleton_core.dart';
 
@@ -6,6 +7,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final SharedPreferences sharedPreferences =
       await SharedPreferences.getInstance();
+  //Remove this method to stop OneSignal Debugging 
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.setAppId("00787a2d-2fd9-4828-982a-4117513b5d6c");
+
+  // The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+      print("Accepted permission: $accepted");
+  });
   runApp(SkeletonCore(
     sharedPreferences: sharedPreferences,
   ));
